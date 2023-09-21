11bb00bc0899e2513e56159a6fbef0e5e471602a
kiran6055 <kirandevopskumar@gmail.com>
2023-09-21 11:52:40 +0530

    all

--- variables.tf
+++ variables.tf
@@ -1,8 +1,21 @@
 variable "tags" {
   type = map
-  default = {
-    Name = "kiran-devops"
-    env = "prod"
-  }
 }
 
+
+variable "security_group_tags" {
+  type = map
+  default = {} # this is optional if we give default here means we can mention there if also not reqd
+}
+
+variable "security_group_name" {
+  type = string
+}
+
+variable "security_group_description" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
