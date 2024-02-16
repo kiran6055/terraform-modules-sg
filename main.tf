c4b673c921c87588fd53c34963d0d010b9cc417e
root <root@ip-172-31-43-190.ap-south-1.compute.internal>
2024-02-16 15:30:56 +0000

    al

--- main.tf
+++ main.tf
@@ -20,12 +20,12 @@ resource "aws_security_group" "main" {
 
 # this resource will add ingress rule to the securoty group
 resource "aws_security_group_rule" "ingress_with_cidr" {
-  count             = lenght(var.ingress_cidr)
+  count             = length(var.ingress_cidr)
   type              = "ingress"
   from_port         = lookup(var.ingress_cidr[count.index], "from_port")
   to_port           = lookup(var.ingress_cidr[count.index], "to_port")
   protocol          = "6" # this is for custom TCP
   cidr_blocks       = [lookup(var.ingress_cidr[count.index], "cidr_blocks")]
   description       = lookup(var.ingress_cidr[count.index], "description")
-  security_group_id = aws_security_group.timing.id
+  security_group_id = aws_security_group.main.id
 }
